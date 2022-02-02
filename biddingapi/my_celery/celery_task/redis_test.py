9cbd199f378a146fc4f71f7d890b288ca0d4320b
yutu-75 <58562109+yutu-75@users.noreply.github.com>
2022-02-02 21:43:31 +0800

    Update redis_test.py

--- biddingapi/my_celery/celery_task/redis_test.py
+++ biddingapi/my_celery/celery_task/redis_test.py
@@ -1,14 +1,14 @@
 import redis
 
 
-# r = redis.StrictRedis(host='47.108.162.205',password='500237', port=6379, db=2)
-r = redis.StrictRedis(host='49.4.31.249',password='listendata315*', port=6379, db=1)
+# r = redis.StrictRedis(host='127.0.0.1',password='500237', port=6379, db=2)
+r = redis.StrictRedis(host='127.0.0.1',password='123456', port=6379, db=1)
 a = r.set('foo', 'bar')
 # print(a)
 print(r.flushall())
 
 
-r = redis.StrictRedis(host='49.4.31.249',password='listendata315*', port=6379, db=2)
+r = redis.StrictRedis(host='127.0.0.1',password='123456', port=6379, db=2)
 a = r.set('foo', 'bar')
 # print(a)
 print(r.flushall())
