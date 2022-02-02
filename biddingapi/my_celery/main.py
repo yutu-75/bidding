0f9ef3cf0ec1dd9cadeccb84139c508d50ab5dde
yutu-75 <58562109+yutu-75@users.noreply.github.com>
2022-02-02 21:42:34 +0800

    Update main.py

--- biddingapi/my_celery/main.py
+++ biddingapi/my_celery/main.py
@@ -8,12 +8,12 @@ django.setup()
 
 # print(import_module('config'))
 
-app = Celery('my_celery.main', broker='redis://:listendata315*@49.4.31.249/2')
+app = Celery('my_celery.main', broker='redis://:123456@127.0.0.1/2')
 
 app.conf.ONCE = {
   'backend': 'celery_once.backends.Redis',
   'settings': {
-    'url': 'redis://:listendata315*@49.4.31.249:6379/1',
+    'url': 'redis://:123456@127.0.0.1:6379/1',
     'default_timeout': 60 * 60 * 3
   }
 }
@@ -27,8 +27,8 @@ app.autodiscover_tasks(['my_celery.celery_task',])
 
 
 # python main.py
-
-# celery -A my_celery.main beat -l info
+# 爬虫代码启动命令
+# celery -A my_celery.main beat -l info                               
 # celery -A my_celery.main worker -l info --pool=solo
 #
 #
